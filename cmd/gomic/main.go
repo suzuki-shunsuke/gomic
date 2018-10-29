650918694d23cbe9f4a058ca506d758386d51891
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-10-29 21:29:57 +0900

    refactor: fix file and function name

--- cmd/gomic/main.go
+++ cmd/gomic/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	handler.Gomock()
+	handler.Gomic()
 }
--- internal/handler/main.go
+++ internal/handler/main.go
@@ -1,1 +1,24 @@
 package handler
+
+import (
+	"os"
+
+	"github.com/urfave/cli"
+
+	"github.com/suzuki-shunsuke/gomic/internal/domain"
+)
+
+// Gomic calls a command.
+func Gomic() {
+	app := cli.NewApp()
+	app.Name = "gomic"
+	app.Version = domain.Version
+	app.Author = "suzuki-shunsuke https://github.com/suzuki-shunsuke"
+	app.Usage = "generate golang's mock for test"
+
+	app.Commands = []cli.Command{
+		InitCommand,
+		GenCommand,
+	}
+	app.Run(os.Args)
+}
