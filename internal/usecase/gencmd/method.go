870a9b04e5609e79ceecb51882e98f3ef7e9c0c6
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-11-07 19:50:44 +0900

    fix: remove name of SetFake** method's internal function's parameters and results

--- internal/usecase/gencmd/method.go
+++ internal/usecase/gencmd/method.go
@@ -37,23 +37,32 @@ func getMethodFromFuncType(
 	method.decl = fmt.Sprintf("%s %s", method.name, s)
 	method.definition = s[4:]
 	if len(results) != 0 {
-		method.setFakeInternalDefinition = s[4:]
 		arr := make([]*ast.Field, len(funcType.Results.List))
+		noNamedResultArr := make([]*ast.Field, len(funcType.Results.List))
 		for i, result := range funcType.Results.List {
 			names := result.Names
 			if len(result.Names) == 0 {
 				names = []*ast.Ident{ast.NewIdent(fmt.Sprintf("r%d", i))}
 			}
-			arr[i] = &ast.Field{
-				Names: names,
-				Type:  result.Type,
-			}
+			arr[i] = &ast.Field{Names: names, Type: result.Type}
+			noNamedResultArr[i] = &ast.Field{Type: result.Type}
+		}
+		noNamedParamArr := make([]*ast.Field, len(funcType.Params.List))
+		for i, param := range funcType.Params.List {
+			noNamedParamArr[i] = &ast.Field{Type: param.Type}
 		}
 		setFakeStr, err := toString(&ast.FuncType{Params: &ast.FieldList{List: arr}})
 		if err != nil {
 			return method, nil, err
 		}
 		method.setFakeDefinition = setFakeStr[4:]
+		s, err := toString(&ast.FuncType{
+			Params:  &ast.FieldList{List: noNamedParamArr},
+			Results: &ast.FieldList{List: noNamedResultArr}})
+		if err != nil {
+			return method, nil, err
+		}
+		method.setFakeInternalDefinition = s[4:]
 	}
 	return method, imports, nil
 }
